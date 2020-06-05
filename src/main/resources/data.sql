4889efc68540dffc5eaeaa9120e4b9810b55e0b2
HouJie <houjie_jack@outlook.com>
2020-06-05 18:14:10 +0800

    启动微服务时默认添加两个admin帐号

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,5 @@
 
 
 truncate table user;
-insert into user (email, password, name, type) values ('admin@outlook.com', '123456', 'admin', 'admin');
+insert into user (email, password, name, type) values ('admin1@outlook.com', '123456', 'admin1', 'admin');
+insert into user (email, password, name, type) values ('admin2@outlook.com', '123456', 'admin2', 'admin');
