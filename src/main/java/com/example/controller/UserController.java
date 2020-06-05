b887bebb1403f66fc096dde6a0c0c9fe17e91303
HouJie <houjie_jack@outlook.com>
2020-06-05 11:04:24 +0800

    业务逻辑转移至service层

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,12 +1,8 @@
 package com.example.controller;
 
-import com.example.model.UserRepository;
+import com.example.service.UserService;
 import com.example.model.User;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,21 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
-  private static final Logger log = LoggerFactory.getLogger(UserController.class);
-
   @Autowired
-  private UserRepository userRepository;
+  private UserService userService;
 
   @GetMapping(path = "/add") // Map ONLY GET REQUESTs.
   public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email,
       @RequestParam String password, User user) {
     // @ResponseBody means the returned String is a response, not a view name.
-    user.setName(name);
-    user.setEmail(email);
-    user.setPassword(password);
-    userRepository.save(user);
-    log.info(user.toString() + " saved to the repo");
-    return "Saved";
+    return userService.add(name, email, password, user);
   }
 
   /**
@@ -42,35 +31,8 @@ public class UserController {
    * @return
    */
   @GetMapping(path = "/login")
-  public String login(@RequestParam String email, @RequestParam String password, Model model) {
-    List<User> users = userRepository.findByEmail(email);
-    // 如果数据库中未查到该账号:
-    if (users == null) {
-      log.warn("attempting to log in with the non-existed account");
-      return "该用户不存在";
-    } else {
-      User user = users.get(0);
-      if (user.getPassword().equals(password)) {
-        // 如果密码与邮箱配对成功:
-        model.addAttribute("name", user.getName());
-        log.warn(user.toString() + " logged in");
-      } else {
-        // 如果密码与邮箱不匹配:
-        model.addAttribute("name", "logging failed");
-        log.warn(user.toString() + " failed to log in");
-      }
-      return "index";
-    }
-  }
-
-  /**
-   * 查看所有用户的注册信息，按照Spring Boot的设定，以Json的形式输送给用户端。
-   * 
-   * @return
-   */
-  @GetMapping(path = "/all")
-  public @ResponseBody Iterable<User> getAllUsers() {
-    return userRepository.findAll();
+  public String logIn(@RequestParam String email, @RequestParam String password, Model model) {
+    return userService.logIn(email, password, model);
   }
 
   /**
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,54 @@
+package com.example.service;
+
+import com.example.model.UserRepository;
+import com.example.model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.springframework.ui.Model;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+
+@Service
+public class UserService {
+  private static final Logger log = LoggerFactory.getLogger(UserService.class);
+  @Autowired
+  private UserRepository userRepository;
+
+  public String add(String name, String email, String password, User user) {
+    List<User> users = userRepository.findByEmail(email);
+    if (users != null) {
+      log.warn("用户账号保存失败，邮箱已注册");
+      return "注册失败，此邮箱已注册";
+    } else {
+      user.setName(name);
+      user.setEmail(email);
+      user.setPassword(password);
+      userRepository.save(user);
+      log.info(user.toString() + "保存至数据库");
+      return "注册成功";
+    }
+  }
+
+  public String logIn(String email, String password, Model model) {
+    List<User> users = userRepository.findByEmail(email);
+    // 如果数据库中未查到该账号:
+    if (users == null) {
+      log.warn("attempting to log in with the non-existed account");
+    } else {
+      User user = users.get(0);
+      if (user.getPassword().equals(password)) {
+        // 如果密码与邮箱配对成功:
+        model.addAttribute("name", user.getName());
+        log.warn(user.toString() + " logged in");
+      } else {
+        // 如果密码与邮箱不匹配:
+        model.addAttribute("name", "logging failed");
+        log.warn(user.toString() + " failed to log in");
+      }
+    }
+    return "index";
+  }
+
+}
