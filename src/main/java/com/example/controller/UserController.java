cd891f98c139c661b07a04ecb07efcc6f8c383f9
HouJie <houjie_jack@outlook.com>
2020-06-06 16:47:13 +0800

    统一使用@RequestMapping注解

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -10,16 +10,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
   @Autowired
   private UserService userService;
 
+
+    /**
+   * 域名根目录进入index.html
+   * 
+   * @param name
+   * @return
+   */
+  @RequestMapping(method = RequestMethod.GET)
+  public String welcomePage(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
+    return "index";
+  }
+
   // 用户注册：验证邮箱是否已经注册
   // 返回值注解@ResponseBody 直接把字符串作为显示内容
-  @PostMapping(path = "/add")
+  @RequestMapping(value="/add", method = RequestMethod.POST)
   public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email,
       @RequestParam String password, User user) {
     return userService.addNewUser(name, email, password, user);
@@ -33,26 +50,15 @@ public class UserController {
    * @param model    Model层存储处理结果，再由View层渲染。
    * @return
    */
-  @PostMapping(path = "/login")
+  @RequestMapping(value="/login", method = RequestMethod.POST)
   public String logIn(@RequestParam String email, @RequestParam String password, Model model) {
     return userService.logIn(email, password, model);
   }
 
-  /**
-   * 域名根目录进入index.html
-   * 
-   * @param name
-   * @return
-   */
-  @GetMapping(path = "/")
-  public String welcomePage(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
-    return "index";
-  }
 
-  @GetMapping("/user/{userId}")
-  public @ResponseBody User getUsers(@RequestParam(name = "userId") Long userId){
+  @RequestMapping(value="/{userId}", method = RequestMethod.GET)
+  public User getUsers(@PathVariable(name = "userId") Long userId){
     return userService.findOne(userId);
   }
-
 
 }
