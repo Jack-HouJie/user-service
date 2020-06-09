a649eedc7d1e34d05791bf42a17602408a0b57a6
HouJie <houjie_jack@outlook.com>
2020-06-09 15:37:45 +0800

    更新管理员帐号

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,4 @@
 
 truncate table user;
 insert into user (email, password, name, type, gender, age, occupation, zipcode) values ('admin1@outlook.com', '123456', 'admin1', 'admin','M','24','1','100044');
-insert into user (email, password, name, type, gender, age, occupation, zipcode) values ('admin2@outlook.com', '123456', 'admin2', 'admin','M','24','1','100044');
+insert into user (email, password, name, type, gender, age, occupation, zipcode) values ('admin2@outlook.com', '123456', 'admin2', 'admin','F','24','5','100044');
